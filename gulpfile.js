87b25a8a412983db203936ea431df6d4379f6bd0
Ryan Tsao <ryan.j.tsao@gmail.com>
2015-02-08 16:01:25 -0800

    Switched to less, added more tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,43 @@
+var del = require('del');
 var path = require('path');
 var gulp = require('gulp');
-var jade = require('gulp-jade');
 var data = require('gulp-data');
+var jade = require('gulp-jade');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
 
 var pages = {
   dir: 'src/pages',
-  glob: '**/*.jade'
+  glob: '**/*.jade',
+  dest: 'dist'
 };
 
-gulp.task('pages', function() {
+gulp.task('pages', ['clean'], function() {
   return gulp.src(path.join(pages.dir, pages.glob))
     .pipe(data(function(file) {
       var relativeDir = path.relative(pages.dir, path.dirname(file.path))
-      var page = path.join(relativeDir, path.basename(file.path, '.jade'));
-      return {page: page};
+      var pageName = path.join(relativeDir, path.basename(file.path, '.jade'));
+      return {page: pageName};
     }))
     .pipe(jade())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(pages.dest));
 });
+
+var styles = {
+  dir: 'src/styles',
+  glob: 'main.less',
+  dest: 'dist/stylesheets'
+};
+
+gulp.task('styles', ['clean'], function() {
+  return gulp.src(path.join(styles.dir, styles.glob))
+    .pipe(less({paths:['node_modules/normalize.css']}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(styles.dest));
+});
+
+gulp.task('clean', function(cb) {
+  del(['dist'], cb);
+});
+
+gulp.task('build', ['pages', 'styles']);
